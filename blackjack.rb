1d07a8c9c1b35106461853fb60587b273fbc9759
Scott Semple <scott@scottsemple.com>
2013-07-12 15:15:51 -0600

    Re-start OOP blackjack from scratch.

--- blackjack.rb
+++ blackjack.rb
@@ -1,19 +1,13 @@
-# Extract the major nouns out of specifications:
-#   Card, Deck, Hand, Dealer, Player
-# Organize behaviors and states into the classes
-
-# CARDS, DECKS AND HANDS
+require 'pry'
 
 class Card
   attr_accessor :value, :suit
 
-  # STATE: Each card has a Value and a Suit
-  def initialize (value, suit)
-    @suit = suit
+  def initialize(value, suit)
     @value = value
+    @suit = suit
   end
 
-  # BEHAVIOR: Each card can be described (to_s).
   def to_s
     "#{value} of #{suit}"
   end
@@ -22,170 +16,181 @@ end
 class Deck
   attr_accessor :cards
 
-  # STATE: Each deck has 52 cards, one for each value and suit combination
   def initialize
     @cards = []
-    values = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13')
-    suits  = ['Spades', 'Hearts', 'Clubs', 'Diamonds']
-    values.product(suits).each do |arr|
-      self.cards << Card.new(arr[0], arr[1])
+    values = ['2', '3', '4', '5', '6', '7', '8', '9', '10',
+              'Jack', 'Queen', 'King', 'Ace']
+    suits = ['Spades', 'Hearts', 'Clubs', 'Diamonds']
+    values.product(suits).each do |card|
+      @cards << Card.new(card[0], card[1])
     end
+    cards.shuffle!
   end
 
-  # BEHAVIOR: A Deck can be shuffled
-  def shuffle
-
+  def deal_one
+    cards.pop
   end
 end
 
-class Hand
-  attr_accessor :cards
-
-  # STATE: Each Hand starts with two cards
-  def initialize
-    @cards = []
+module Hand
+  def show_hand
+    puts ""
+    puts "---- #{name}'s Hand ----"
+    cards.each do |card|
+      puts "#{card}"
+    end
+    puts "Total: #{total_hand}"
   end
 
-  # BEHAVIOR: A Hand can be dealt
-  def deal_cards
-
-  end
-
-  # BEHAVIOR: A Hand can be hit
-  def hit
-
-  end
-
-  # BEHAVIOR: A Hand can be described
-  def to_s
-    "#{cards}"
-  end
-
-  # BEHAVIOR: A Hand can be totaled
   def total_hand
-=begin
-    total = 0
+    values = cards.map { |card| card.value }
 
-    hand.each do |card|
-      total += card
+    total = 0
+    values.each do |value|
+      if value == 'Ace'
+        total += 11
+      elsif value.to_i == 0
+        total += 10
+      else
+        total += value.to_i
+      end
     end
 
-    hand.select { |e| e == 11 }.count.times do
-      if total > 21
+    # Correct for Aces
+    values.select { |value| value == 'Ace' }.count.times do
+      if total <= 21
+        break
+      else
         total -= 10
       end
     end
-    
+
     total
-=end
+  end
+
+  def add_card(new_card)
+    cards << new_card
+  end
+
+  def is_busted?
+    total_hand > 21
   end
 end
 
-# PLAYER PLAY
+class Player
+  include Hand
 
-class Player 
-  attr_accessor :name, :hand
+  attr_accessor :name, :cards
 
-  # STATES: A Player starts with a name and two cards
-  def initialize
+  def initialize(name)
     @name = name
-    @hand = @hand
+    @cards = []
+  end
+end
+
+class Dealer
+  include Hand
+
+  attr_accessor :name, :cards
+
+  def initialize
+    @name = "Dealer"
+    @cards = []
   end
 
-  # BEHAVIOR: A Player can have a turn
+end
+
+class Blackjack
+  attr_accessor :deck, :player, :dealer
+
+  def initialize
+    @deck = Deck.new
+    @player = Player.new("")
+    @dealer = Dealer.new
+  end
+
+  def get_player_name
+    puts ""
+    puts "Let's play blackjack!"
+    puts "What's your name?"
+    player.name = gets.chomp
+  end
+
+  def deal_cards
+    player.add_card(deck.deal_one)
+    player.add_card(deck.deal_one)
+    player.show_hand
+    dealer.add_card(deck.deal_one)
+    dealer.add_card(deck.deal_one)
+    dealer.show_hand
+  end
+
   def player_turn
-=begin
-    while player_score < 21
-      say "Hit or stay? (Type '1' for a new card or 'Enter' to stay.)"
+    while player.is_busted? != true
+      puts ""
+      puts "Hit or stay? (Type '1' for a new card or 'Enter' to stay.)"
       response = gets.chomp
       if response.to_i == 1
-        new_card = deal
-        player_hand << new_card
-        say "Your new card is #{new_card}."
-        player_score = total_hand(player_hand)
-          if player_score > 21
-            say 'You bust.'
-            break
-          else
-            say "Your hand is now worth #{player_score}."
+        player.add_card(deck.deal_one)
+        puts ""
+        puts "Your new card is a #{player.cards[-1].to_s}."
+        player.show_hand
+          if player.is_busted? == true
+            puts 'You bust.'
+            play_again?
           end
       else
         break
       end
     end
-=end
-  end
-end
-
-# DEALER PLAY
-
-class Dealer
-  attr_accessor :hand
-
-  def initialize
-    @hand = @hand
   end
 
   def dealer_turn
-=begin
-    while dealer_score < 21
-      if dealer_score <= 16
-        new_card = deal
-        dealer_hand << new_card
-        say "The dealer hits and gets a #{new_card}."
-        dealer_score = total_hand(dealer_hand)
-        if dealer_score > 21
-          say 'The dealer busts.'
-        else
-          say "The dealer has #{dealer_score}."
+    while dealer.is_busted? != true
+      if dealer.total_hand <= 16
+      dealer.add_card(deck.deal_one)
+      puts ""
+      puts "The dealer hits and gets a #{dealer.cards[-1].to_s}."
+      dealer.show_hand
+        if dealer.is_busted? == true
+          puts 'The dealer busts.'
         end
       else
         break
       end
     end
-=end
   end
-end
 
-# GENERAL GAME STRUCTURE
+  def who_won?
+    puts ""
+    if dealer.is_busted? != true && dealer.total_hand >= player.total_hand
+      puts 'Dealer wins.'
+    else
+      puts 'You win!'
+    end
+  end
 
-class Blackjack
-  attr_accessor
-
-  def initialize
-    @deck = Deck.new
-    @player = Player.new ("Scott")
-    @dealer = Dealer.new
-    @hand = Hand.new
+  def play_again?
+    puts ""
+    puts "Would you like to play again? (Type 'y' for a new card " +
+         "or 'Enter' to quit.)"
+    response = gets.chomp
+    if response == "y"
+      game = Blackjack.new
+      game.start
+    else
+      exit
+    end
   end
 
   def start
+    get_player_name
     deal_cards
-    # say "You: #{player_hand[0]} and #{player_hand[1]} for a hand of #{player_score}."
-    # say "Dealer: #{dealer_hand[0]} and #{dealer_hand[1]} for a hand of #{dealer_score}."
     player_turn
-    # if player_score <= 21
-    #   say "Now it's the dealer's turn."
-    # end
     dealer_turn
     who_won?
     play_again?
   end
-
-  def deal_cards
-
-  end
-
-  def who_won
-=begin
-  if dealer_score <= 21 && dealer_score >= player_score
-    say 'Dealer wins.'
-  else
-    say 'You win!'
-  end
-=end
-  end
-
 end
 
 game = Blackjack.new
