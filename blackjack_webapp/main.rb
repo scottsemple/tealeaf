971604fb7cd8b321bd582a8b20ea0981bd61a463
Scott Semple <scott@scottsemple.com>
2013-07-17 13:50:01 -0600

    Push app with Week 3 challenges.

--- blackjack_webapp/main.rb
+++ blackjack_webapp/main.rb
@@ -3,19 +3,80 @@ require 'sinatra'
 
 set :sessions, true
 
+helpers do
+  def total_hand(cards)
+    values = cards.map { |card| card[0] }
+
+    total = 0
+    values.each do |value|
+      if value == 'Ace'
+        total += 11
+      elsif value.to_i == 0
+        total += 10
+      else
+        total += value.to_i
+      end
+    end
+
+    # Correct for Aces
+    values.select { |value| value == 'Ace' }.count.times do
+      if total <= 21
+        break
+      else
+        total -= 10
+      end
+    end
+
+    total
+  end
+end
+
+
 get '/' do
-  '...some text in the browser.'
+  if session[:player_name]
+    redirect "/game"
+  else
+    redirect "/new_player"
+  end
 end
 
-get '/template' do
- erb :mytemplate
+get "/new_player" do
+  erb :new_player
 end
 
-get '/nested-template' do
-  erb :"users/profile"
+post "/new_player" do
+  session[:player_name] = params[:player_name]
+  redirect "/game"
 end
 
-get '/not-here' do
-  redirect '/'
+get "/game" do
+  # deck
+  values = ['2', '3', '4', '5', '6', '7', '8', '9', '10',
+            'Jack', 'Queen', 'King', 'Ace']
+  suits = ['Spades', 'Hearts', 'Clubs', 'Diamonds']
+  session[:deck] = values.product(suits).shuffle!
+
+  # deal_cards
+  session[:player_hand] = []
+  session[:dealer_hand] = []
+  session[:player_hand] << session[:deck].pop
+  session[:dealer_hand] << session[:deck].pop
+  session[:player_hand] << session[:deck].pop
+  session[:dealer_hand] << session[:deck].pop
+
+  erb :game
 end
 
+post "/game/player/hit" do
+  session[:player_hand] << session[:deck].pop
+  if total_hand(session[:player_hand]) > 21
+    @error = "Sorry, you busted."
+  end
+
+  erb :game
+end
+
+post "/game/player/stay" do
+  @success = "You have chosen to stay."
+  erb :game
+end
